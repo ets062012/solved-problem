b6323cd35b3a519305f25fd8fadf4548b830acfa
tarikua <tarikuamisganaw@gmail.com>
2023-10-28 16:23:38 +0300

    valid-palindrme

---
+++ validpalindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        newstr=""
+        for c in s:
+            if c.isalnum():
+                newstr+=c.lower()
+        return newstr==newstr[::-1]
