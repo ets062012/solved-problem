e8d44cd7977005109a072e710689700d68dfb6d7
tarikua <88793839+ets062012@users.noreply.github.com>
2023-12-14 16:31:48 +0300

    Added solution for Sum of Even Numbers After Queries

---
+++ solved/sum-of-even-numbers-after-queries.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        result = []
+        even_sum = sum(x for x in nums if x % 2 == 0)
+    
+        for val, index in queries:
+            if nums[index] % 2 == 0:
+                even_sum -= nums[index]
+            nums[index] += val
+            if nums[index] % 2 == 0:
+                even_sum += nums[index]
+            result.append(even_sum)
+    
+        return result
+        
